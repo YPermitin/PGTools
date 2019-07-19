d3f4626883ec6c9c9484dbbf0d59bb5a9c7fd699
Permitin Yury <ypermitin@yandex.ru>
2019-07-20 02:23:40 +0500

    Добавил запрос информации размера таблиц

---
+++ SQL-Server-Databases-Info/Размеры таблиц.sql
@@ -0,0 +1,11 @@
+SELECT
+	tablename AS table_name,
+	pg_class.reltuples as rows,
+	pg_total_relation_size(schemaname||'.'||tablename) / 1024 AS reservedKB,
+	pg_table_size(schemaname||'.'||tablename) / 1024 AS dataKB,
+	pg_indexes_size(schemaname||'.'||tablename) / 1024 as index_sizeKB,
+	0 as unusedKB
+FROM pg_catalog.pg_tables, pg_catalog.pg_class
+where pg_tables.tablename = pg_class.relname  
+	and schemaname = 'public' 
+ORDER BY pg_total_relation_size(schemaname||'.'||tablename) DESC

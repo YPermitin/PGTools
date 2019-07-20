e4c161b31c72915343bcc96b1024e72246c86ae8
Permitin Yury <ypermitin@yandex.ru>
2019-07-20 14:58:51 +0500

    Исправил запрос инфо размера таблиц и индексов

--- PG-Databases-Info/Размеры таблиц.sql
+++ PG-Databases-Info/Размеры таблиц.sql
@@ -4,7 +4,9 @@ SELECT
 	pg_total_relation_size(schemaname||'.'||tablename) / 1024 AS reservedKB,
 	pg_table_size(schemaname||'.'||tablename) / 1024 AS dataKB,
 	pg_indexes_size(schemaname||'.'||tablename) / 1024 as index_sizeKB,
-	0 as unusedKB
+	pg_total_relation_size(schemaname||'.'||tablename)
+		- pg_table_size(schemaname||'.'||tablename)
+		- pg_indexes_size(schemaname||'.'||tablename) as unusedKB
 FROM pg_catalog.pg_tables, pg_catalog.pg_class
 where pg_tables.tablename = pg_class.relname  
 	and schemaname = 'public' 

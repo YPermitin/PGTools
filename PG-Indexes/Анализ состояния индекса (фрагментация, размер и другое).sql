204253636b7f9b2844770441fccc007746a128d4
YPermitin <ypermitin@yandex.ru>
2022-12-30 01:07:02 +0500

    Расширен скрипт анализа состояния индексов

--- PG-Indexes/Анализ состояния индекса (фрагментация, размер и другое).sql
+++ PG-Indexes/Анализ состояния индекса (фрагментация, размер и другое).sql
@@ -4,6 +4,10 @@ CREATE EXTENSION pgstattuple;
 */
 
 select
+    -- Схема
+    n.nspname,
+    -- Таблица
+    ti.relname as "TableName",
 	-- Имя индекса
 	i.indexrelid::regclass as "IndexName",
 	-- Фрагментация на уровне листьев
@@ -27,13 +31,15 @@ select
     -- Средняя плотность страниц на уровне листьев
     s.avg_leaf_density,
     -- Средняя плотность страниц на уровне листьев
-    s.avg_leaf_density,
-    i.*
+    s.avg_leaf_density
 FROM pg_index AS i
    JOIN pg_class AS t ON i.indexrelid = t.oid
    JOIN pg_opclass AS opc ON i.indclass[0] = opc.oid
    JOIN pg_am ON opc.opcmethod = pg_am.oid
    CROSS JOIN LATERAL pgstatindex(i.indexrelid) AS s
+   join pg_class ti ON ti.oid = i.indrelid
+   LEFT JOIN pg_namespace n ON n.oid = ti.relnamespace
 WHERE t.relkind = 'i'
   AND pg_am.amname = 'btree'
+  and ti.relkind = ANY (ARRAY['r', 't'])
 order by s.leaf_fragmentation desc

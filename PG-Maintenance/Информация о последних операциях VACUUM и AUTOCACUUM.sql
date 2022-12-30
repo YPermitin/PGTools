136fb4afb533d7feb28558ac4767a3238dc99dfa
YPermitin <ypermitin@yandex.ru>
2022-12-30 14:20:18 +0500

    Добавлен скрипт "Информация о последних операциях VACUUM и AUTOCACUUM"

---
+++ PG-Maintenance/Информация о последних операциях VACUUM и AUTOCACUUM.sql
@@ -0,0 +1,8 @@
+SELECT 
+    -- Объект базы данных
+    relname, 
+    -- Дата последней операции VACUUM
+    last_vacuum, 
+    -- Дата последней операции AUTOVACUUM
+    last_autovacuum 
+FROM pg_stat_user_tables;
